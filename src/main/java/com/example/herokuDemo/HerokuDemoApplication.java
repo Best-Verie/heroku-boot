b89c36888216c80694b91b9dcb19ee047a8b7ee3
Best-Verie <irabestverie2016@gmail.com>
2022-03-09 17:16:17 +0200

    added default endpoint

--- src/main/java/com/example/herokuDemo/HerokuDemoApplication.java
+++ src/main/java/com/example/herokuDemo/HerokuDemoApplication.java
@@ -3,8 +3,15 @@ package com.example.herokuDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Controller
 @SpringBootApplication
 public class HerokuDemoApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuDemoApplication.class, args);
