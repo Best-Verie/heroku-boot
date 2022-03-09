6120a89ade2048ceb89e1c0a48beb40a7517594e
Best-Verie <irabestverie2016@gmail.com>
2022-03-09 18:26:32 +0200

    fix

--- src/main/java/com/example/herokuDemo/HerokuDemoApplication.java
+++ src/main/java/com/example/herokuDemo/HerokuDemoApplication.java
@@ -2,6 +2,9 @@ package com.example.herokuDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
 
 @SpringBootApplication
 public class HerokuDemoApplication {
