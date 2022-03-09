4a59baef8dd30b254416ff6f933bf6ec0b6aa450
Best-Verie <irabestverie2016@gmail.com>
2022-03-09 18:39:51 +0200

    done

--- src/main/java/com/example/herokuDemo/HerokuDemoApplication.java
+++ src/main/java/com/example/herokuDemo/HerokuDemoApplication.java
@@ -3,12 +3,11 @@ package com.example.herokuDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-
 @Controller
 
 @SpringBootApplication
 public class HerokuDemoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuDemoApplication.class, args);
 	}
